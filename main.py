01dca2996831def90ef2bdb64f69180ccfaa7bca
E A <emmanuelalade2008@gmail.com>
2023-04-28 15:54:23 +0200

    .

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from turtle import Screen, Turtle
 from player import SpaceShooter
+import time
 
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(700, 700)
+screen.tracer(0)
 
 
 player = SpaceShooter()
@@ -11,7 +13,18 @@ player = SpaceShooter()
 
 
 
+screen.listen()
+screen.onkey(player.left, "Left")
+screen.onkey(player.right, "Right")
 
+
+
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    player.shoot()
 
 
 
--- player.py
+++ player.py
@@ -1,10 +1,32 @@
 from turtle import Turtle
-
+import random
 
 
 class SpaceShooter(Turtle):
     def __init__(self):
         super().__init__()
+        self.lives = 3
         self.shape("square")
         self.color("green")
-        self.shapesize(2, 4)
+        self.penup()
+        self.goto(0, -320)
+        self.shapesize(2, 6)
+
+    def left(self):
+        new_x = self.xcor() - 10
+        self.goto(new_x, -320)
+    
+    def right(self):
+        new_x = self.xcor() + 10
+        self.goto(new_x, -320)
+    
+
+    def shoot(self):
+        MOVE = 10
+        bullet = Turtle(shape="circle")
+        bullet.color("red")
+        bullet.penup()
+        bullet.setpos(0, 0)
+        shot = random.randint(1, 4)
+        if shot == 1:
+            bullet.forward(MOVE)
