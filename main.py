870474cc8b25d607212b320c4a0795de871f0607
E A <emmanuelalade2008@gmail.com>
2023-04-28 14:48:16 +0200

    Player modification

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from turtle import Screen, Turtle
+from player import SpaceShooter
 
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(700, 700)
 
 
+player = SpaceShooter()
 
 
 
--- player.py
+++ player.py
@@ -1,1 +1,10 @@
 from turtle import Turtle
+
+
+
+class SpaceShooter(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("square")
+        self.color("green")
+        self.shapesize(2, 4)
